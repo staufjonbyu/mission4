c84a60063fa24094c9f5fba6ad148f0566f9a6fb
Jon Stauffer <staufjon@byu.edu>
2023-02-06 23:16:53 -0700

    newstage

---
+++ mission4/Controllers/FirstController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mission4.Models;
+
+namespace mission4.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Calculator(CalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
